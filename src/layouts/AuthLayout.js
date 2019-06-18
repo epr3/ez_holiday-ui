9f4b4ee5bcd8a8cbba8bed7a89d2e50874c3c6dc
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-06-18 22:36:54 +0300

    added navbar

---
+++ src/layouts/AuthLayout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useStoreActions } from 'easy-peasy';
+import { Pane, Button, Heading } from 'evergreen-ui';
+
+function AuthLayout({ children }) {
+  const logout = useStoreActions(actions => actions.auth.logout);
+
+  return (
+    <>
+      <Pane display="flex" padding={16} background="tint2" borderRadius={3}>
+        <Pane flex={1} alignItems="center" display="flex">
+          <Heading size={600}>EZ Holiday</Heading>
+        </Pane>
+        <Pane>
+          <Button marginRight={16} onClick={() => logout()}>
+            Log out
+          </Button>
+        </Pane>
+      </Pane>
+      <Pane display="flex" padding={16}>
+        {children}
+      </Pane>
+    </>
+  );
+}
+
+export default AuthLayout;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import AuthLayout from '../layouts/AuthLayout';
+
 function Home() {
-  return <p>Home</p>;
+  return (
+    <AuthLayout>
+      <p>Home</p>
+    </AuthLayout>
+  );
 }
 
 export default Home;
