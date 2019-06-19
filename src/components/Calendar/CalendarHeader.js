0c13ab2f385658d74209eb4f2d0361788d45a36a
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-06-19 20:35:28 +0300

    calendar header styling fix

--- src/components/Calendar/CalendarHeader.js
+++ src/components/Calendar/CalendarHeader.js
@@ -9,9 +9,19 @@ function CalendarHeader({
 }) {
   return (
     <Pane display="flex" justifyContent="center" padding={16}>
-      <IconButton icon="chevron-left" onClick={previousMonthAction} />
-      <Button appearance="minimal">{currentDateTime.monthLong}</Button>
-      <IconButton icon="chevron-right" onClick={nextMonthAction} />
+      <IconButton
+        icon="chevron-left"
+        onClick={previousMonthAction}
+        marginRight={16}
+      />
+      <Button disabled isActive={false} appearance="minimal">
+        {currentDateTime.monthLong}
+      </Button>
+      <IconButton
+        icon="chevron-right"
+        onClick={nextMonthAction}
+        marginLeft={16}
+      />
     </Pane>
   );
 }
