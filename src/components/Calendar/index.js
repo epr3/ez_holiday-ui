e64d1fb398cad177a4ff5363219f832377978fe5
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-06-20 22:57:01 +0300

    refactor of calendar hooks

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -16,58 +16,54 @@ const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(item => (
   <Table.TextHeaderCell key={item}>{item}</Table.TextHeaderCell>
 ));
 
+const buildCalendar = dt => {
+  const currentDate = DateTime.local();
+  const startOfMonth = dt.startOf('month');
+  const daysInMonth = [];
+
+  for (let i = 1; i <= dt.daysInMonth; i++) {
+    const currentDayCondition =
+      currentDate.hasSame(startOfMonth.plus({ days: i - 1 }), 'month') &&
+      currentDate.day === startOfMonth.plus({ days: i - 1 }).day;
+    daysInMonth.push(
+      <Table.TextCell
+        background={currentDayCondition ? 'redTint' : ''}
+        key={`day-${i}`}
+      >
+        {i}
+      </Table.TextCell>
+    );
+  }
+
+  const rows = [];
+  let cells = [];
+  const totalSlots = [...generateBlanks(startOfMonth.weekday), ...daysInMonth];
+
+  totalSlots.forEach((row, i) => {
+    if (i % 7 !== 0 || i === 0) {
+      cells.push(row);
+    } else {
+      rows.push(cells);
+      cells = [];
+      cells.push(row);
+    }
+
+    if (i === totalSlots.length - 1) {
+      rows.push(cells);
+    }
+  });
+  console.log('here');
+  return rows.map((d, i) => (
+    <Table.Row key={`row-${i}`} justifyContent="flex-start">
+      {d}
+      {generateBlanks(7 - d.length + 1)}
+    </Table.Row>
+  ));
+};
+
 function Calendar() {
   const [dt, setDt] = useState(DateTime.local());
   const [table, setTable] = useState([]);
-
-  const buildCalendar = dt => {
-    const currentDate = DateTime.local();
-    const startOfMonth = dt.startOf('month');
-    const daysInMonth = [];
-
-    for (let i = 1; i <= dt.daysInMonth; i++) {
-      const currentDayCondition =
-        currentDate.hasSame(startOfMonth.plus({ days: i - 1 }), 'month') &&
-        currentDate.day === startOfMonth.plus({ days: i - 1 }).day;
-      daysInMonth.push(
-        <Table.TextCell
-          background={currentDayCondition ? 'redTint' : ''}
-          key={`day-${i}`}
-        >
-          {i}
-        </Table.TextCell>
-      );
-    }
-
-    const rows = [];
-    let cells = [];
-    const totalSlots = [
-      ...generateBlanks(startOfMonth.weekday),
-      ...daysInMonth
-    ];
-
-    totalSlots.forEach((row, i) => {
-      if (i % 7 !== 0 || i === 0) {
-        cells.push(row);
-      } else {
-        rows.push(cells);
-        cells = [];
-        cells.push(row);
-      }
-
-      if (i === totalSlots.length - 1) {
-        rows.push(cells);
-      }
-    });
-    setTable(
-      rows.map((d, i) => (
-        <Table.Row key={`row-${i}`} justifyContent="flex-start">
-          {d}
-          {generateBlanks(7 - d.length + 1)}
-        </Table.Row>
-      ))
-    );
-  };
 
   const nextMonth = () => {
     setDt(dt.plus({ months: 1 }));
@@ -78,7 +74,7 @@ function Calendar() {
   };
 
   useEffect(() => {
-    buildCalendar(dt);
+    setTable(buildCalendar(dt));
   }, [dt]);
 
   return (
